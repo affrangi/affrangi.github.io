cde0cbf8a903f50dfe0901a8890f58492be386ea
bfrangi <bernat.frangi@gmail.com>
2025-06-25 20:55:10 +0000

    Deploying to gh-pages from @ affrangi/affrangi.github.io@8351067698c39c644da6a0419d54da3ae8ba5a57 🚀

---
+++ academic-cv-tools/compile.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Get the directory of the current script
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+# Process the cv data
+echo "Processing education data..."
+python3 "$SCRIPT_DIR/src/process_education.py" "$SCRIPT_DIR/input/config.json"
+echo "Processing experience data..."
+python3 "$SCRIPT_DIR/src/process_experience.py" "$SCRIPT_DIR/input/config.json"
+echo "Processing honors and awards data..."
+python3 "$SCRIPT_DIR/src/process_honors_awards.py" "$SCRIPT_DIR/input/config.json"
+echo "Processing scholarships and fellowships data..."
+python3 "$SCRIPT_DIR/src/process_scholarships_fellowships.py" "$SCRIPT_DIR/input/config.json"
+
+# Join the cv data
+echo "Joining CV data..."
+python3 "$SCRIPT_DIR/src/join.py" "$SCRIPT_DIR/input/basics.json" "$SCRIPT_DIR/output/experience.json" \
+    "$SCRIPT_DIR/output/education.json" "$SCRIPT_DIR/output/fellowships.json" "$SCRIPT_DIR/output/honors.json"
+
+# Process the funding data
+echo "Processing funding data..."
+python3 "$SCRIPT_DIR/src/process_funding.py" "$SCRIPT_DIR/input/config.json"
+
+# Process the publications data
+echo "Processing publications data..."
+python3 "$SCRIPT_DIR/src/process_publications.py" "$SCRIPT_DIR/input/config.json"
---
+++ academic-cv-tools/src/lib/config.py
@@ -0,0 +1,34 @@
+from lib.files import input_dir, output_dir
+
+
+def read_bib_config(config_path: str) -> tuple[list[str], list[str]]:
+    """Read the BibTeX config file and return the sources and categories."""
+    import json
+
+    with open(config_path, "r") as file:
+        config = json.load(file)
+
+    input_directory = input_dir()
+    data = {
+        f"{input_directory}/{source['file']}": source["category"]
+        for source in config["bibliography"]
+        if "file" in source and "category" in source
+    }
+
+    return list(data.keys()), list(data.values())
+
+
+def read_cv_config(config_path: str, section: str) -> tuple[str, str]:
+    """Read the CV config file and return the input and output paths."""
+    import json
+
+    with open(config_path, "r") as file:
+        config = json.load(file)
+
+    input_file = config[section]
+    output_file = input_file.replace(".tex", ".json")
+
+    input_path = f"{input_dir()}/{input_file}"
+    output_path = f"{output_dir()}/{output_file}"
+
+    return input_path, output_path
